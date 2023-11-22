d4ef93b3eabaf3eae4034453de9c2020076a03a0
Michael Ernst <mernst@cs.washington.edu>
2023-11-22 13:21:04 -0800

    Mike's code-review comments

--- scripts/compile-project.sh
+++ scripts/compile-project.sh
@@ -1,46 +1,105 @@
 #!/bin/sh
 
-# Compile the project that contains the current directory.
-# Usage:
-#   compile-project
+###
+### This file is copied from
+### https://github.com/plume-lib/manage-git-branches/blob/main/compile-project
+###
+
+# Compile the project that contains the current or given directory.
 #
-# Supports Gradle, Maven, and Make projects.
+# Supports Gradle, Maven, and Make projects.  If no buildfile is found
+# at the top level, searches at (only) the next level down.
+#
+# The exit status is failure if any build fails.  If no buildfile is
+# found and ERR_IF_NO_BUILDFILE is set to a non-empty value, then the
+# exit status is failure.
+#
+# Usage:
+#   compile-project [<directory>]
+#
 # If variable GRADLE_ASSEMBLE_FLAGS is defined, it is passed to `gradle assemble`.
 # If variable MVN_COMPILE_FLAGS is defined, it is passed to `mvn compile`.
 # If variable MAKE_FLAGS is defined, it is passed to `make`.
+# If variable ERR_IF_NO_BUILDFILE is non-empty, the script fails if no buildfile is found.
 #
 # This command does not need to be run in the top-level directory of the project;
 # it will discover the top-level directory on its own and run the command there.
 
-# TODO: Handle other build systems too.
+# TODO: Handle other build systems too, such as Ant.
 # TODO: Have a list of per-directory or per-repository commands, to override the default.
 
-if [ "$#" -ne 0 ]; then
-  echo "Usage: $(basename "$0")" >&2
+# $toplevel does not have a trailing "/" character
+if [ "$#" -eq 0 ]; then
+  if toplevel="$(git rev-parse --show-toplevel 2>&1)" ; then
+    echo "Running $(basename "$0") in $toplevel, called from $(pwd)"
+  else
+    toplevel=$(pwd)
+    echo "Running $(basename "$0") in $toplevel"
+  fi
+elif [ "$#" -eq 1 ]; then
+  toplevel="$1"
+  echo "Running $(basename "$0") in $toplevel"
+else
+  echo "Usage: $(basename "$0") [<directory>]" >&2
   exit 1
 fi
 
-echo "Running compile-project in $(pwd)"
+# Returns a status code indicating success or failure, or 222 if no buildfile was found.
+compile_in_directory() {
+  if [ "$#" -ne 1 ] ; then
+    echo "compile_in_directory got $# arguments: $*"
+    exit 1
+  fi
+  dir="$1"
 
-# shellcheck disable=SC2034 # Variable not used yet.
-toplevel="$(pwd)"
+  if [ -f "$dir/gradlew" ]; then
+    # shellcheck disable=SC2086 # Word splitting is desirable here.
+    "$dir/gradlew" --project-dir "$dir" assemble -q ${GRADLE_ASSEMBLE_FLAGS} < /dev/null
+    return $?
+  elif [ -f "$dir/build.gradle" ]; then
+    # shellcheck disable=SC2086 # Word splitting is desirable here.
+    gradle --project-dir "$dir" assemble -q ${GRADLE_ASSEMBLE_FLAGS} < /dev/null
+    return $?
+  elif [ -f "$dir/mvnw" ]; then
+    # shellcheck disable=SC2086 # Word splitting is desirable here.
+    (cd "$dir" && ./mvnw -q compile ${MVN_COMPILE_FLAGS})
+    return $?
+  elif [ -f "$dir/pom.xml" ]; then
+    # shellcheck disable=SC2086 # Word splitting is desirable here.
+    (cd "$dir" && mvn -q compile ${MVN_COMPILE_FLAGS})
+    return $?
+  elif [ -f "$dir/Makefile" ]; then
+    # shellcheck disable=SC2086 # Word splitting is desirable here.
+    (cd "$dir" && make ${MAKE_FLAGS})
+    return $?
+  else
+    # No buildfile was found.
+    return 222
+  fi
+}
 
-if [ -f "$toplevel/gradlew" ]; then
-  # shellcheck disable=SC2086 # Word splitting is desirable here.
-  "$toplevel/gradlew" --project-dir "$toplevel" assemble -q ${GRADLE_ASSEMBLE_FLAGS}
-elif [ -f "$toplevel/build.gradle" ]; then
-  # shellcheck disable=SC2086 # Word splitting is desirable here.
-  gradle --project-dir "$toplevel" assemble -q ${GRADLE_ASSEMBLE_FLAGS}
-elif [ -f "$toplevel/mvnw" ]; then
-  # shellcheck disable=SC2086 # Word splitting is desirable here.
-  (cd "$toplevel" && ./mvnw -q compile ${MVN_COMPILE_FLAGS})
-elif [ -f "$toplevel/pom.xml" ]; then
-  # shellcheck disable=SC2086 # Word splitting is desirable here.
-  (cd "$toplevel" && mvn -q compile ${MVN_COMPILE_FLAGS})
-elif [ -f "$toplevel/Makefile" ]; then
-  # shellcheck disable=SC2086 # Word splitting is desirable here.
-  (cd "$toplevel" && make ${MAKE_FLAGS})
+compile_in_directory "$toplevel"
+result=$?
+
+if [ "$result" != 222 ] ; then
+  # A buildfile was found; exit with the exit code of the compilation step.
+  exit $result
 else
-  echo "compile-project did nothing in $(pwd)"
+  # Call compile_in_directory in subdirectories.
+  buildfile_found=0
+  for subdir in "$toplevel"/*/ ; do
+    compile_in_directory "$subdir"
+    result=$?
+    if [ "$result" = 222 ] ; then
+      continue
+    fi
+    buildfile_found=1
+    if [ ! $result ] ; then
+      exit $result
+    fi
+  done
+  if [ "$buildfile_found" = 0 ] && [ -n "$ERR_IF_NO_BUILDFILE" ]; then
+    echo "compile-project did nothing in $(pwd)"
+    exit 1
+  fi
 fi
-
--- scripts/mutation.sh
+++ scripts/mutation.sh
@@ -1,68 +1,72 @@
 #!/bin/bash
 
-#This script uses mutation testing with Randoop generating the test suites. Different
-#test suites can be generated with Bloodhound, Orienteering, neither (baseline), or both.
-#Mutation testing is used on projects provided in table 2 of the GRT paper.
+# This script does mutation testing with Randoop-generated test suites. Different
+# test suites can be generated with Bloodhound, Orienteering, neither (baseline), or both.
+# Mutation testing is used on projects provided in table 2 of the GRT paper.
 
-#This script will create and put Randoop's test suites in a "test" subdirectory. 
-#Compiled tests and code will be stored in a "bin" subdirectory.
-#The script will generate various mutants of the source project using Major and run these tests on those mutants.
+# This script will create Randoop's test suites in a "test" subdirectory. 
+# Compiled tests and code will be stored in a "bin" subdirectory.
+# The script will generate various mutants of the source project using Major and run these tests on those mutants.
 
-#Finally, each experiment can run a given amount of times and a given amount of seconds per class. 
-#Each iteration will be logged to a file "info.txt".
-#See "repoinstructions.txt" for more instructions on how to successfully run this script and reproduce my results.
+# Finally, each experiment can run a given amount of times and a given amount of seconds per class. 
+# Each iteration will be logged to a file "info.txt".
+# See "reproinstructions.txt" for more instructions on how to run this script.
 
 make
 
-#Link to the major directory
-MAJOR_HOME="../major/"
-MAJOR_HOME=$(realpath "$MAJOR_HOME")
+# Link to the major directory
+MAJOR_HOME=$(realpath "../major/")
 
-#Link to the randoop jar
-RANDOOP_JAR="jarfiles/randoop-all-4.3.2.jar"
-RANDOOP_JAR=$(realpath "$RANDOOP_JAR")
+# Link to the randoop jar
+RANDOOP_JAR=$(realpath "jarfiles/randoop-all-4.3.2.jar")
 
-#Link to src files containing the project (must be Gradle, Maven, or a Make project)
+# TODO: The following should be command-line arguments.
+# Link to src files containing the project (must be Gradle, Maven, or a Make project)
 PROJECT_SRC="/mnt/c/Users/varun/Downloads/commons-cli-1.2-src/commons-cli-1.2-src"
 
-#Link to java src files (exclude subdirectory w/ test files)
+# Link to java src files (exclude subdirectory w/ test files)
 JAVA_SRC_FILES="/mnt/c/Users/varun/Downloads/commons-cli-1.2-src/commons-cli-1.2-src/src/java"
 
-#Link to jacoco agent jar
-JACOCO_JAR="jarfiles/lib/jacocoagent.jar"
-JACOCO_JAR=$(realpath "$JACOCO_JAR")
+# Link to jacoco agent jar
+JACOCO_JAR=$(realpath "jarfiles/lib/jacocoagent.jar")
 
-#Link to original directory
-CURR_DIR=$(pwd)
-CURR_DIR=$(realpath "$CURR_DIR")
+# Link to original directory
+CURR_DIR=$(realpath "$(pwd)")
 
-#Seconds per class
+# TODO: This is a crazily low value for Randoop.  Is this just temporary, for testing?
+# Seconds per class
 SECONDS_CLASS="2"
 
-#Number of times to run experiments (10 in GRT paper)
+# Number of times to run experiments (10 in GRT paper)
 NUM_LOOP=2
 
+# TODO: This file is important to the for loop, but not until then.  So put the code about it just above the for loop.
 rm info.txt
 touch info.txt
-rm -rf $PROJECT_SRC/target #Specific to Apache Commons Cli v 1.2 and its pom.xml file -- will have to generalize in the future
+rm -rf $PROJECT_SRC/target # Specific to Apache Commons Cli v 1.2 and its pom.xml file -- will have to generalize in the future
 
 echo "Using Randoop to generate tests"
 echo
-cd $PROJECT_SRC && $CURR_DIR/compile-project.sh && cd $CURR_DIR
+(cd $PROJECT_SRC && "$CURR_DIR"/compile-project.sh)
 
-PROJECT_SRC="$PROJECT_SRC/target/classes" #Again, specific to Apache Commons Cli v 1.2 and its pom.xml file -- will have to generalize in the future
+# TODO: Please put all variables at the top of the file, rather than interspersing them with code.
+# TODO: Put any variables that a user might want to set (or that will eventually be set from command-line arguments) at the beginnig of the variables.
+PROJECT_SRC="$PROJECT_SRC/target/classes" # Again, specific to Apache Commons Cli v 1.2 and its pom.xml file -- will have to generalize in the future
 
 find $PROJECT_SRC -type f -name "*.class" -printf "%P\n" | sed 's/\//./g' | sed 's/.class$//' > $PROJECT_SRC/myclasses.txt
 
 NUM_CLASSES=$(wc -l < $PROJECT_SRC/myclasses.txt)
 TIME_LIMIT=$((NUM_CLASSES * SECONDS_CLASS))
 
-#Variable that stores command line inputs common among all commands
+# Variable that stores command line inputs common among all commands
 CLI_INPUTS="java -Xbootclasspath/a:$JACOCO_JAR -javaagent:$JACOCO_JAR -classpath $PROJECT_SRC:$RANDOOP_JAR randoop.main.Main gentests --classlist=$PROJECT_SRC/myclasses.txt --time-limit=$TIME_LIMIT"
 
+# shellcheck disable=SC2034 # i counts iterations but is not otherwise used.
 for i in $(seq 1 $NUM_LOOP)
 do
     rm -rf test*
+
+    # TODO: There should eventually be a command-line argument that chooses among the variants of Ranndoop.
 
     echo "Using Bloodhound"
     echo
@@ -92,18 +96,18 @@ do
     echo "Compiling and mutating project"
     echo "(ant -Dmutator=\"=mml:\$MAJOR_HOME/mml/all.mml.bin\" clean compile)"
     echo
-    $MAJOR_HOME/bin/ant -Dmutator="mml:$MAJOR_HOME/mml/all.mml.bin" -Dsrc="$JAVA_SRC_FILES" clean compile
+    "$MAJOR_HOME"/bin/ant -Dmutator="mml:$MAJOR_HOME/mml/all.mml.bin" -Dsrc="$JAVA_SRC_FILES" clean compile
     
     echo
     echo "Compiling tests"
     echo "(ant compile.tests)"
     echo
-    $MAJOR_HOME/bin/ant -Dtest="$TEST_DIRECTORY" -Dsrc="$JAVA_SRC_FILES" compile.tests
+    "$MAJOR_HOME"/bin/ant -Dtest="$TEST_DIRECTORY" -Dsrc="$JAVA_SRC_FILES" compile.tests
 
     echo
     echo "Run tests with mutation analysis"
     echo "(ant mutation.test)"
-    $MAJOR_HOME/bin/ant -Dtest="$TEST_DIRECTORY" mutation.test
+    "$MAJOR_HOME"/bin/ant -Dtest="$TEST_DIRECTORY" mutation.test
 
     cat summary.csv >> info.txt
    
