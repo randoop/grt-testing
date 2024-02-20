77dca7d089eeda42d5a14b9261f2015cd5032038
varuniy <varuniy@cs.washington.edu>
2024-02-20 15:13:04 -0800

    Added faulty script for ClassViewer jarfile

---
+++ scripts/ClassViewer-5.0.5b.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script does mutation testing with using the ClassViewer-5.0.5b project.
+
+dir="../tests/src/ClassViewer-5.0.5b"
+repo="https://github.com/randoop/grt-ClassViewer-5.0.5b.git"
+jarfile="../tests/ClassViewer-5.0.5b.jar"
+srcDir="../tests/src/ClassViewer-5.0.5b/"
+
+make
+
+if [ ! -d "$dir" ]; then
+    git clone "$repo" "$dir"
+fi
+
+./mutation.sh "$jarfile" "$srcDir" ""
