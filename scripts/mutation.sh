71e3798655400c6590a82970f09496b1510597a2
Michael Ernst <mernst@cs.washington.edu>
2024-05-16 11:19:10 -0700

    Add CI

--- scripts/mutation.sh
+++ scripts/mutation.sh
@@ -20,10 +20,14 @@ MAJOR_HOME=$(realpath "build/major/")
 CURR_DIR=$(realpath "$(pwd)")
 
 # Link to the randoop jar
-RANDOOP_JAR=$(realpath "build/randoop-all-4.3.2.jar")
 RANDOOP_JAR1="/Users/ziheji/Desktop/academic/Research/randoop/build/libs/randoop-all-4.3.2.jar"
 RANDOOP_JAR2="/scratch/mernst/clones/testing/randoop-fork-kuro1fury-branch-constant-mining-v2/build/libs/randoop-all-4.3.3.jar"
-RANDOOP_JAR="${RANDOOP_JAR1}:${RANDOOP_JAR2}:${RANDOOP_JAR}"
+RANDOOP_JAR=$(realpath "build/randoop-all-4.3.2.jar")
+if [ -f ${RANDOOP_JAR1} ] ; then
+  RANDOOP_JAR=${RANDOOP_JAR1}
+elif [ -f ${RANDOOP_JAR2} ] ; then
+  RANDOOP_JAR=${RANDOOP_JAR2}
+fi
 
 # Link to jacoco agent jar. This is necessary for Bloodhound
 JACOCO_JAR=$(realpath "build/jacocoagent.jar")
