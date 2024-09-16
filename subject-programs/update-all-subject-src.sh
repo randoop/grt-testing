33abc6c04fc0773edcdb9248806557004c77cdf4
776styjsu <yt776styjsu@gmail.com>
2024-09-15 21:51:14 -0700

    Add script to update subject programs

---
+++ subject-programs/update-all-subject-src.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# shellcheck disable=SC2164
+cd ../subject-programs/src/
+echo -n "a4j-1.0b: "; cd a4j-1.0b; git pull; cd ../
+echo -n "asm-5.0.1: "; cd asm-5.0.1; git pull; cd ../
+echo -n "bcel-5.2: "; cd bcel-5.2; git pull; cd ../
+echo -n "ClassViewer-5.0.5b: "; cd ClassViewer-5.0.5b; git pull; cd ../
+echo -n "commons-cli-1.2: "; cd commons-cli-1.2; git pull; cd ../
+echo -n "commons-codec-1.9: "; cd commons-codec-1.9; git pull; cd ../
+echo -n "commons-collections4-4.0: "; cd commons-collections4-4.0; git pull; cd ../
+echo -n "commons-compress-1.8: "; cd commons-compress-1.8; git pull; cd ../
+echo -n "commons-lang3-3.0: "; cd commons-lang3-3.0; git pull; cd ../
+echo -n "commons-math3-3.2: "; cd commons-math3-3.2; git pull; cd ../
+echo -n "commons-primitives-1.0: "; cd commons-primitives-1.0; git pull; cd ../
+echo -n "dcParseArgs-10.2008: "; cd dcParseArgs-10.2008; git pull; cd ../
+echo -n "easymock-3.2: "; cd easymock-3.2; git pull; cd ../
+echo -n "fixsuite-r48: "; cd fixsuite-r48; git pull; cd ../
+echo -n "guava-16.0.1: "; cd guava-16.0.1; git pull; cd ../
+echo -n "hamcrest-core-1.3: "; cd hamcrest-core-1.3; git pull; cd ../
+echo -n "javassist-3.19: "; cd javassist-3.19; git pull; cd ../
+echo -n "javax.mail-1.5.1: "; cd javax.mail-1.5.1; git pull; cd ../
+echo -n "jaxen-1.1.6: "; cd jaxen-1.1.6; git pull; cd ../
+echo -n "jcommander-1.35: "; cd jcommander-1.35; git pull; cd ../
+echo -n "jdom-1.0: "; cd jdom-1.0; git pull; cd ../
+echo -n "joda-time-2.3: "; cd joda-time-2.3; git pull; cd ../
+echo -n "JSAP-2.1: "; cd JSAP-2.1; git pull; cd ../
+echo -n "jvc-1.1: "; cd jvc-1.1; git pull; cd ../
+echo -n "nekomud-r16: "; cd nekomud-r16; git pull; cd ../
+echo -n "pmd-core-5.2.2: "; cd pmd-core-5.2.2; git pull; cd ../
+echo -n "sat4j-core-2.3.5: "; cd sat4j-core-2.3.5; git pull; cd ../
+echo -n "shiro-core-1.2.3: "; cd shiro-core-1.2.3; git pull; cd ../
+echo -n "slf4j-api-1.7.12: "; cd slf4j-api-1.7.12; git pull; cd ../
+echo -n "tinySQL-2.26: "; cd tinySQL-2.26; git pull; cd ../
