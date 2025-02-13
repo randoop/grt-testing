9904c3b834b31e8b311a2b87cfa532d67f50fac3
Yash Mathur <yashmat05@gmail.com>
2025-02-13 12:22:45 -0800

    simplification

--- scripts/mutation.sh
+++ scripts/mutation.sh
@@ -82,7 +82,7 @@ CLASSPATH="$(echo "$JAR_DIR"/*.jar | tr ' ' ':')"
 # The feature names must not contain whitespace.
 ALL_RANDOOP_FEATURES=("BASELINE" "BLOODHOUND" "ORIENTEERING" "BLOODHOUND_AND_ORIENTEERING" "DETECTIVE" "GRT_FUZZING" "ELEPHANT_BRAIN" "CONSTANT_MINING")
 # The different features of Randoop to use. Adjust according to the features you are testing.
-RANDOOP_FEATURES=("BASELINE") #"ORIENTEERING" "BLOODHOUND_AND_ORIENTEERING" "DETECTIVE" "GRT_FUZZING" "ELEPHANT_BRAIN" "CONSTANT_MINING")
+RANDOOP_FEATURES=("BASELINE") #"BLOODHOUND" "ORIENTEERING" "BLOODHOUND_AND_ORIENTEERING" "DETECTIVE" "GRT_FUZZING" "ELEPHANT_BRAIN" "CONSTANT_MINING")
 
 # When ABLATION is set to false, the script tests the Randoop features specified in the RANDOOP_FEATURES array.
 # When ABLATION is set to true, each run tests all Randoop features except the one specified in the RANDOOP_FEATURES array.
