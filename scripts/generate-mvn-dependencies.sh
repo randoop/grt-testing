13f4e1bb0352074d91c08c203be4cb5b94bb2f9e
Varun Iyengar <varuniy@uw.edu>
2025-05-14 00:10:50 -0700

    Removed dynamic generation of pom.xml and renamed build.xml to build-randoop.xml

--- scripts/generate-mvn-dependencies.sh
+++ scripts/generate-mvn-dependencies.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
-# For each .jar file in ./libs/, this script:
-#  * installs the .jar file into the local Maven repository and
-#  * adds it as a dependency to a pom.xml file.
+# For each .jar file in build/lib/, this script:
+#  * installs the .jar file into the local Maven repository
 
 # The path to the lib directory.
 LIBS_DIR="build/lib"
-
-# The path to pom.xml.
-POM_FILE="program-config/$1/pom.xml"
 
 # Installs necessary packages to maven for all subject programs
 mvn install:install-file -Dfile="build/org.jacoco.agent-0.8.0-runtime.jar" -DgroupId="org.jacoco" -DartifactId="org.jacoco.agent" -Dversion="0.8.0" -Dclassifier="runtime" -Dpackaging=jar
 mvn install:install-file -Dfile="build/evosuite-standalone-runtime-1.2.0.jar" -DgroupId="org.evosuite" -DartifactId="evosuite-standalone-runtime" -Dversion="1.2.0" -Dpackaging=jar
 mvn install:install-file -Dfile="build/junit-4.12.jar" -DgroupId="com.example" -DartifactId="junit" -Dversion="4.12" -Dpackaging=jar
 mvn install:install-file -Dfile="build/hamcrest-core-1.3.jar" -DgroupId="com.example" -DartifactId="hamcrest-core" -Dversion="1.3" -Dpackaging=jar
-
-# Backup the pom.xml before modifying it
-cp -pf "$POM_FILE" "build/pom.xml.bak"
-echo "Backup of pom.xml created at build/pom.xml.bak"
 
 if ! find "$LIBS_DIR" -maxdepth 1 -name '*.jar' | grep -q .; then
   echo "No JAR files found in $LIBS_DIR. Skipping."
@@ -40,52 +32,6 @@ for jar in "$LIBS_DIR"/*.jar; do
   mvn install:install-file -Dfile="$jar" -DgroupId="$groupId" -DartifactId="$artifactId" -Dversion="$version" -Dpackaging=jar
   echo "Installed $filename into local Maven repository"
 
-  # Check if the dependency is already in the pom.xml
-  if grep -q "<artifactId>$artifactId</artifactId>" "$POM_FILE"; then
-    echo "Dependency for $artifactId is already in pom.xml. Skipping addition."
-  else
-    # Create a temporary file for the dependency
-    TEMP_FILE=$(mktemp)
-
-    # Create the corresponding <dependency> entry for the pom.xml
-    cat << EOF > "$TEMP_FILE"
-  <dependency>
-    <groupId>$groupId</groupId>
-    <artifactId>$artifactId</artifactId>
-    <version>$version</version>
-EOF
-
-    # Add exclusion if the dependency is log4j-1.2.15
-    if [[ "$groupId" == "com.example" && "$artifactId" == "log4j" && "$version" == "1.2.15" ]]; then
-      cat << EOF >> "$TEMP_FILE"
-    <exclusions>
-      <exclusion>
-        <groupId>com.sun.jmx</groupId>
-        <artifactId>jmxri</artifactId>
-      </exclusion>
-      <exclusion>
-        <groupId>com.sun.jdmk</groupId>
-        <artifactId>jmxtools</artifactId>
-      </exclusion>
-      <exclusion>
-        <groupId>javax.jms</groupId>
-        <artifactId>jms</artifactId>
-      </exclusion>
-    </exclusions>
-EOF
-    fi
-
-    # Close the <dependency> tag
-    echo "  </dependency>" >> "$TEMP_FILE"
-
-    # Insert the dependency before the closing </dependencies> tag
-    sed -i -e "/<\/dependencies>/r $TEMP_FILE" -e "x; \$ {s/<\/dependencies>//; p; x}; 1d" "$POM_FILE"
-
-    # Clean up the temporary file
-    rm -f "$TEMP_FILE"
-
-    echo "Added dependency for $artifactId-$version to pom.xml"
-  fi
 done
 
-echo "All JARs have been installed and dependencies added to pom.xml."
+echo "All JARs have been installed to the local maven repository."
--- scripts/mutation-evosuite.sh
+++ scripts/mutation-evosuite.sh
@@ -485,6 +485,7 @@ for i in $(seq 1 "$NUM_LOOP"); do
     exec 1>> "mutation_output.txt" 2>&1
   fi
 
+  echo $EVOSUITE_COMMAND -Dtest_dir="$TEST_DIRECTORY" -Dreport_dir="$REPORT_DIRECTORY"
   $EVOSUITE_COMMAND -Dtest_dir="$TEST_DIRECTORY" -Dreport_dir="$REPORT_DIRECTORY"
 
   # After test generation, for JSAP-2.1, we need to remove the ant.jar from the classpath
@@ -592,9 +593,6 @@ done
 #===============================================================================
 # Build System Cleanup
 #===============================================================================
-# Restore the original pom.xml
-mv -f "build/pom.xml.bak" "program-config/$1/pom.xml"
-
 echo "Restoring $JAVA_SRC_DIR to main branch"
 # switch to main branch (may already be there)
 (
